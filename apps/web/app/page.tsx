d55329b33aa8baf7ee83cccbc2c54cf7c94e7560
Mahesh Reddy <yellareddymaheshreddy@gmail.com>
2025-04-08 21:24:01 +0530

    small change

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>apps/web/app/page.tsx</code>
+            Ihave changed this <code>apps/web/app/page.tsx</code>
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
